ca1b0a5dddc229c7a04d3750f596ad91a4053b99
Lucas Block <lucas.block-medin@grenoble-in.orgp>
2021-12-01 09:38:08 +0100

    Modif de class Con et verification de mail

--- src/gange/Conn.java
+++ src/gange/Conn.java
@@ -2,14 +2,14 @@ package gange;
 
 import java.sql.*;
 
-public class Connection {
+public class Conn {
 
     static final String CONN_URL = "jdbc:oracle:thin:@oracle1.ensimag.fr:1521:oracle1";
-    private String user = "faccinhg"; // A remplacer pour votre compte, sinon genere une exception
-    private String password = "faccing";
-    public Connection conn;
+    private String user = "blockmel"; // A remplacer pour votre compte, sinon genere une exception
+    private String password = "blockmel";
+    public Connection connection;
     
-    public Connection() {
+    public Conn() {
         try {
         // Enregistrement du driver Oracle
         System.out.print("Loading Oracle driver... "); 
@@ -25,7 +25,12 @@ public class Connection {
         }  
     }
     
-    public close() {
+    public void close() {
+        if(conn != null){
+            try{
+                conn.close();
+            }catch (SQLException e){/*we can ignore this exception*/}
+        }
     	
     }
     
--- src/gange/Menu.java
+++ src/gange/Menu.java
@@ -14,10 +14,34 @@ public class Menu {
         String email = scan.next();
         //verify if email exist
         //if doesnt exist ask again to user to put a valid email
+        while(verifyEmail(email)){
+            System.out.print("S'il vous plait rentrez une adresse mail valide:"); 
+            String email = scan.next();
+        }
         System.out.print("password: "); 
         String password = scan.next();        
         scan.close(); 
         // verify login and if it is right set email and password
+
+    }
+
+    public boolean verifyEmail(String email){
+        try{
+            //On établit la connection avec la BDD
+            Conn conn= new Conn();
+            //Creation de la requete
+            PreparedStatement verifyEmail = conn.connection.prepareStatement("select * from  client where email = ?");
+            verifyEmail.setString(1, email); 
+            ResultSet rset = stmt.executeQuery();
+            // Fermeture 
+            rset.close();
+            verifyEmail.close();
+            conn.close();
+            return true;
+        }catch(Exception e){
+            System.err.println("Cette adresse mail n'existe pas");
+            return false;
+        }
     }
 
     
