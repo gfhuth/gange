3df3b5a623735e5ef898e9675944482a15d36194
Guilherme Faccin Huth <guilherme.faccin-huth@grenoble-inp.org>
2021-12-04 19:11:36 +0000

    Atualizar src/gange/User.java

--- src/gange/User.java
+++ src/gange/User.java
@@ -4,8 +4,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class User {
+	private String email;
 
-	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 }
