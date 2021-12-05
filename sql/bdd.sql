7b2c2972dc74639fe7a1fcc29ad12d29b11e79f6
Lucas Block <lucas.block-medin@grenoble-inp.org>
2021-12-05 16:33:12 +0100

    RGPD faite. Il reste des TODO à voir mais l'élimination et la création de comptes est gérée

---
+++ sql/bdd.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Utilisateur
+(
+id_u INT NOT NULL,
+PRIMARY KEY(id_u)
+);
+
+CREATE TABLE Client
+(
+email	       varchar(255) NOT NULL,
+id_u	       REFERENCES UTILISATEUR(id_u),
+nom            varchar (30) NOT NULL,
+prenom         varchar (30) NOT NULL,
+adresse        varchar(255) NOT NULL,
+Mot_de_passe   varchar (30) NOT NULL,
+PRIMARY KEY (email)
+);
+
+CREATE TABLE CATEGORIE (
+nom_cat varchar(255) NOT NULL,
+cat_mere varchar(255) NOT NULL,
+PRIMARY KEY (nom_cat)
+);
+
+CREATE TABLE PRODUIT
+(
+id_p	       INT NOT NULL,
+nom_cat        REFERENCES CATEGORIE(nom_cat),
+intitulé       varchar (30) NOT NULL,
+prix_courant   FLOAT NOT NULL,
+description    varchar(255) NOT NULL,
+url_photo      varchar(255) NOT NULL,
+PRIMARY KEY (id_p)
+);
+
+CREATE TABLE DONNEES
+(
+Caracteristique varchar(255) NOT NULL,
+id_p            REFERENCES PRODUIT(id_p),
+valeur          varchar(255) NOT NULL,
+PRIMARY KEY (Caracteristique)
+);
+
+CREATE TABLE OFFRE
+(
+id_p          REFERENCES PRODUIT(id_p),
+datee         varchar(255) NOT NULL,
+heure         varchar(255) NOT NULL,
+id_u          REFERENCES UTILISATEUR(id_u),
+prix_proposé  FLOAT NOT NULL,
+num_offre     INT NOT NULL,
+PRIMARY KEY(datee, heure),
+CONSTRAINT   prix_proposé  CHECK   (prix_proposé > 0),
+CONSTRAINT   num_offre     CHECK   (num_offre < 6)
+);
--- src/gange/Menu.java
+++ src/gange/Menu.java
@@ -2,6 +2,7 @@ package gange;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Scanner;
+import java.util.LinkedList;
 
 public class Menu {
 	
@@ -72,7 +73,77 @@ public class Menu {
 
 	}
 
-	public void deletUser(String user) {
+	/**
+	 * Cette fonction va servir à gérer un Client qui veut éliminer son compte
+	 * 
+	 * TODO : Il serait intéréssant de stocker le mail du client lors du login,parceque ça n'as pas trop de sens de le redemander.
+	 * 
+	 * @param c
+	 * @param email
+	 */
+	public void eliminerClient(ConnectionManager c, String email) {
+		Scanner scan = new Scanner(System.in);  
+		System.out.print("Bonjour, êtes vous sur de vouloir éliminer votre compte? [oui/non]");   
+		String confirmation = scan.nextLine();
+		System.out.println(confirmation);
+		if(confirmation.equals("oui")){
+			if(c.delClient(email)){
+				System.out.println("Votre compte a ete elimine. On va vous déconnecter");
+				c.close();
+			}else{
+				System.out.println("Il y a eu une erreur. On n'as pas pu vous éliminer.");
+			}	
+		}else{
+			System.out.println("On reviens vers votre page de recommandations");
+			System.out.println("Si c'est ps ce que vous vouliez,veuillez reéssayer en écrivant oui en toutes lettres");
+		}
+		scan.close();
+	}
+
+	/**
+	 * Fonction qui permet de créer un compte en demandant les informations au client
+	 * 
+	 * TODO : Il serait intéressant de vérifier les coords fournies. Vérification de si un compte existe déjà
+	 * Et vérification que les coords sont logiques
+	 * 
+	 * @param c
+	 */
+	public void creationCompte(ConnectionManager c){
+		LinkedList<String> coord = new LinkedList<String>();
+		Scanner scan = new Scanner(System.in); 
+
+		//On recupère les coords
+		System.out.print("On va procéder à la création d'un compte veuillez répondre aux questions suivantes\n");
+		System.out.println("Email:");
+		String email=scan.nextLine();
+		System.out.println(email);
+		coord.add(email);
+		System.out.println("Nom:");
+		String nom=scan.nextLine();
+		System.out.println(nom);
+		coord.add(nom);
+		System.out.println("Prenom:");
+		String prenom=scan.nextLine();
+		System.out.println(prenom);
+		coord.add(prenom);
+		System.out.println("Adresse:");
+		String adresse=scan.nextLine();
+		System.out.println(adresse);
+		coord.add(adresse);
+		System.out.println("Mot de passe:");
+		String motDePasse=scan.nextLine();
+		System.out.println(motDePasse);
+		coord.add(motDePasse);
+
+		//On execute la demande de création
+		if(c.createClient(coord)){
+			System.out.println("Votre compte a été crée correctement. Maintenant vous êtes connecté");
+		}else{
+			System.out.println("Une erreur est survenue, le compte n'as pas été créé");
+		}
+
+		//On ferme le scan
+		scan.close();
 	}
 }
 
