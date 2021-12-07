86ba57123e16d00929921ea8be7d9d23ebf9215b
gfhuth <ghuth@disroot.org>
2021-12-07 23:01:01 +0100

    main update

--- src/main.java
+++ src/main.java
@@ -6,24 +6,34 @@ public class main {
 		ConnectionManager conn = new ConnectionManager();
 		Menu m = new Menu(conn);
 
-		switch(m.login()) {
-		case 1:
-			switch(m.askSuggestion()) {
+		switch(m.loginOrSignUp()) {
+		case 1://m.loginOrSignUp()
+			switch(m.login()) {
 			case 1:
-				System.out.println("oi"); 	//with suggestion
+				switch(m.askSuggestion()) {
+				case 1:
+					System.out.println("oi"); 	//with suggestion
+					break;
+				case 2:
+					System.out.println("thcau"); // without suggestion
+					break;
+				default: break;
+				}//m.askSuggestion()
 				break;
-			case 2:
-				System.out.println("thcau"); // without suggestion
-				
-				
+			case 0: break;
+			default:
+				System.out.println("au revoir");
+				conn.close();
 				break;
-			default: break;
-			}
-		case 0:
-		default:
+			}//m.login()
+		case 2://m.loginOrSignUp()
+			m.creationCompte();
+			break;
+		default://m.loginOrSignUp()
+			m.closeMenu();
 			System.out.println("au revoir");
 			conn.close();
 			break;
-		}
+		}//m.loginOrSignUp()
 	}
 }
