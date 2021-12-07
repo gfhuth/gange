8a5e6fd6fa83a18068b162d265488a1e81892272
Yassine Katof <yassine.katof@grenoble-inp.org>
2021-12-07 17:34:29 +0000

    Update produit.sql

--- sql/produit.sql
+++ sql/produit.sql
@@ -1,9 +1,54 @@
-
 INSERT INTO produit VALUES (
-	'1', 
+	8, 
 	'Chaussures', 
 	'Chaussures noir', 
 	'5', 
-	' Dimensions du produit(L x l x h): ‎40.6 x 17.8 x 12.7 cm; 311.84 grammes Date de mise en ligne sur : ‎ 1 juillet 2015 ', 
+	' Dimensions du produit(L x l x h): 40.6 x 17.8 x 12.7 cm; 311.84 grammes Date de mise en ligne sur :  1 juillet 2015 ', 
 	'https://images-eu.ssl-images-amazon.com/images/I/8125fvykgXL.__AC_SY395_SX395_QL70_ML2_.jpg'
 );
+
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES(
+	1, 
+	'Chausson d''escalade', 
+	'Chausson d''escalade-ROCK', 
+	35, 
+	'Confortable et vous aide à vous améliorer rapidement',
+    'https://www.decathlon.fr/p/chaussons-d-escalade-rock/_/R-p-8899?mc=8230651&
+	 gclid=EAIaIQobChMIt5ftvdTR9AIVg7HtCh0bkg8GEAQYBiABEgK_3fD_BwE');
+ 
+ 
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES
+(2, 'Chaussures de Tennis', 'Chaussure Tennis Femme', 60, 'très moderne se marie facilement',
+ 'https://www.decathlon.fr/p/chaussure-tennis-femme-advantage-base-blanche-rose/_/R-p-X8621578?mc=8621578');
+ 
+ 
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES
+(3, 'T-SHIRT FITNESS', 'TSHIRT FITNESS MANCHES LONGUES', 12, 'bien coupé et stylé',
+ 'https://www.decathlon.fr/p/t-shirt-fitness-manches-longues-slim-coton-extensible-col-rond-homme/_/R-p-323787?mc=8640475');
+ 
+ 
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES
+(4, 'VESTE POLAIRE CHAUDE DE RANDONNE', 'VESTE CHAUDE IMPERIABLE', 40, 'idéale pour randonner dans le froid',
+ 'https://www.decathlon.fr/p/veste-chaude-impermeable-de-randonnee-sh100-x-warm-10-c-femme/_/R-p-166271?mc=8398572');
+ 
+ 
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES
+(5, 'Chausson d''escalade', 'Chausson d''escalade-ROCK+GRIS', 50, 'Confortable et de grande qualité',
+ 'https://www.decathlon.fr/p/chaussons-d-escalade-rock/_/R-p-167271?mc=8495164');
+ 
+ 
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES
+(6, 'Chaussures de Tennis', 'Chaussure Tennis Professionnel', 135, 'très moderne et pratique pour tous types de surface',
+ 'https://www.decathlon.fr/p/chaussures-de-tennis-adulte-gel-solution-speed-ff-blanches-multicourt/_/R-p-X8659342?mc=8659342');
+ 
+ 
+INSERT INTO PRODUIT (ID_P, NOM_CAT, INTITULÉ, PRIX_COURANT, DESCRIPTION, URL_PHOTO)
+VALUES
+(7, 'Chausson d''escalade', 'Chausson d''escalade-EDGE LACET', 90, 'Elaboré avec des grimpeurs experts',
+ 'https://www.decathlon.fr/p/chaussons-d-escalade-edge-lacet-v2-anis-bleu/_/R-p-309268?mc=8550817');
