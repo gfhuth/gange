3e7bbd8a71eeb5b1be9476cadb1f6dd0071f2ce0
Yassine Katof <yassine.katof@grenoble-inp.org>
2021-12-07 17:42:26 +0000

    Update donnee.sql

---
+++ sql/donnee.sql
@@ -0,0 +1,35 @@
+-- DONNEES
+ 
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 1, 'Toutes les tailles allant de 37 à 48 sont disponibles');
+
+
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 2, 'Toutes les tailles allant de 37 à 42 sont disponibles');
+
+
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 3, 'Toutes les tailles allant de S à 4XL sont disponibles');
+
+ 
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 4, 'Toutes les tailles allant de S à 2XL sont disponibles');
+
+
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 5, 'Toutes les tailles allant de 37 à 48 sont disponibles');
+
+
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 6, 'Toutes les tailles allant de 37 à 46 sont disponibles');
+
+
+INSERT INTO DONNEES (CARACTERISTIQUE, ID_P, VALEUR)
+VALUES
+('Tailles disponobiles', 7, 'Toutes les tailles allant de 37 à 48 sont disponibles');
