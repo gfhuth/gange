3fa935778f92c9836ed9c4291558888565f605de
Yassine Katof <yassine.katof@grenoble-inp.org>
2021-12-07 15:07:50 +0000

    Update requete.sql

--- sql/requete.sql
+++ sql/requete.sql
@@ -1,1 +1,18 @@
+ --Affiche le nom de la catégorie et le nombre d'offre qui ont été faites dans cette catégorie par ordre decroissant
+ 
+ 
+SELECT nom_cat, COUNT(nom_cat) as nb
+FROM PRODUIT
+INNER JOIN OFFRE on Produit.ID_P = OFFRE.ID_P 
+GROUP BY nom_cat
+ORDER BY nb DESC;
 
+
+--Affiche combien d'offre l'utilsiateur numero 1 a effectué dans toutes les catégeries par ordre décroissant
+ 
+SELECT id_u, nom_cat, COUNT(nom_cat) as nb
+FROM PRODUIT
+INNER JOIN OFFRE on Produit.ID_P = OFFRE.ID_P
+Where offre.id_u = 1
+GROUP BY nom_cat, id_u
+ORDER BY nb DESC;
